ac2f60bba7df90ee8aa74607a1176573f907c4b7
kerryfrog <idasom060@gmail.com>
2023-07-25 10:11:28 +0900

    swea_1249

--- swea_1249.py
+++ swea_1249.py
@@ -1,5 +1,5 @@
-# 최대 거리 9 *100 = 900
-max_int = 1000
+from queue import PriorityQueue
+
 T = int(input())
 
 # 세로: R, 가로: C
@@ -8,52 +8,33 @@ dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
 
-def dfs(now, visited, alpha, cnt):
-    global ans
-    visited[now[0]][now[1]] = 1
-    alpha[ord(board[now[0]][now[1]]) - 65] = 1
-    for i in range(4):
-        new_x = now[0] + dx[i]
-        new_y = now[1] + dy[i]
-        if 0 <= new_y < C and 0 <= new_x < R and visited[new_x][new_y] == 0:
-            if alpha[ord(board[new_x][new_y]) - 65] == 1:
-                continue
-            if cnt + 1 > ans:
-                ans = cnt + 1
-            dfs([new_x, new_y], visited, alpha, cnt + 1)
-            visited[new_x][new_y] = 0
-            alpha[ord(board[new_x][new_y]) - 65] = 0
-    return
+## 우선순위 큐 를 이용한 BFS
+def BFS(board):
+    N = len(board)
+    queue = PriorityQueue()
+    queue.put((0, [0, 0]))
+    visited = [[0 for _ in range(N)] for _ in range(N)]
+    while queue:
+        cost, now = queue.get()
+        x, y = now
+        if x == N - 1 and y == N - 1:
+            return cost
+        if visited[x][y] == 1:
+            continue
+        visited[x][y] = 1
+        for i in range(4):
+            new_x = x + dx[i]
+            new_y = y + dy[i]
+            if 0 <= new_y < N and 0 <= new_x < N and visited[new_x][new_y] == 0:
+                queue.put((cost + board[new_x][new_y], [new_x, new_y]))
 
 
+## 문제 풀이
 for t in range(1, T + 1):
     N = int(input())
-    board = [[max_int] * (N + 1)]
-    ans = [[max_int] * (N + 1)]
+    board = []
     for i in range(N):
         tmp = list(map(int, list(input())))
-        tmp.insert(0, max_int)
-        ans.append(tmp)
         board.append(tmp)
-
-    for i in range(3, 2 * N + 1):
-        for j in range(1, i):
-            if j > N or i - j > N:
-                # print("i=", i, "j=", j, "i-j=", i - j)
-                continue
-            # min value 설정에서도 못나가도록  설정해 주어야 함
-            # print("i=", i, "j=", j, "i-j=", i - j)
-            # print("min value is", board[j - 1][i - j], board[j][i - j - 1])
-            board[j][i - j] += min(board[j - 1][i - j], board[j][i - j - 1])
-    # print(board)
-    print(f"#{t} {board[N][N]}")
-
-
-# 알파벳
-
-
-visited = [[0] * C for _ in range(R)]
-
-for i in range(R):
-    el = list(input())
-    board.append(el)
+    ans = BFS(board)
+    print(f"#{t} {ans}")

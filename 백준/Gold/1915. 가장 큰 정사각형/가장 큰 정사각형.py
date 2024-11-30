699a90ccc937a4141ba528124b22e1dfdb229396
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2024-11-30 20:25:12 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 972 ms, Memory: 64304 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
@@ -7,17 +7,14 @@ dp = [[0 for _ in range(M+1)] for _ in range(N+1)]
 
 
 
+ans = 0
 for i in range(1, N+1):
     for j in range(1, M+1):
         if board[i-1][j-1] == '1':
             tmp = [dp[i-1][j], dp[i-1][j-1], dp[i][j-1]]
             dp[i][j] = min(tmp) + 1
+            ans = max(ans,dp[i][j]  )
 
-
-ans = 0
-for i in range(N+1):
-    for j in range(M+1):
-        ans = max(dp[i][j], ans)
 
 
 print(ans*ans)
